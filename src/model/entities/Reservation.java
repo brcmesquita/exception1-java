60d798a3938494a663cdc0979c1b430b37141ae2
Bruno Raphael Cabral de Mesquita <brcmesquita@gmail.com>
2021-02-04 15:57:41 -0300

    Good solution

--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -1,8 +1,10 @@
-package application.model.entities;
+package model.entities;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
+
+import model.exceptions.DomainException;
 
 public class Reservation {
 
@@ -13,7 +15,10 @@ public class Reservation {
     // vamos precisar criar uma máscara para receber as datas no padrão dd/MM/yyyy
     private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
-    public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
+    public Reservation(Integer roomNumber, Date checkIn, Date checkOut) throws DomainException {
+        if (!checkOut.after(checkIn)) {
+            throw new DomainException("Check-out date must be after check-in date.");
+        }
         this.roomNumber = roomNumber;
         this.checkIn = checkIn;
         this.checkOut = checkOut;
@@ -44,20 +49,18 @@ public class Reservation {
         return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
     }
 
-    public String updateDates(Date checkIn, Date checkOut) {
+    public void updateDates(Date checkIn, Date checkOut) throws DomainException {
 
         Date now = new Date();
         if (checkIn.before(now) || checkOut.before(now)) {
-            return "Reservation dates for update must be future dates.";
+            throw new DomainException("Reservation dates for update must be future dates.");
         }
         if (!checkOut.after(checkIn)) {
-            return "Check-out date must be after check-in date.";
+            throw new DomainException("Check-out date must be after check-in date.");
         }
 
         this.checkIn = checkIn;
         this.checkOut = checkOut;
-
-        return null;
     }
 
     // Agora vamos criar um método ToString para mostrar as informações
